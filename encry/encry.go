dafb0c386dccf4adc2c7b86bb51aea812da0dcd4
BaiYang <xliu779@foxmail.com>
2019-02-01 12:07:49 +0800

    添加redis和aes加密

---
+++ encry/encry.go
@@ -0,0 +1,33 @@
+package encry
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+// AesEncrypt 加密
+func AesEncrypt(origData []byte, keyString string) []byte {
+	k := []byte(keyString)
+
+	// 分组秘钥
+	block, _ := aes.NewCipher(k)
+	// 获取秘钥块的长度
+	blockSize := block.BlockSize()
+	// 补全码
+	origData = PKCS7Padding(origData, blockSize)
+	// 加密模式
+	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
+	// 创建数组
+	cryted := make([]byte, len(origData))
+	// 加密
+	blockMode.CryptBlocks(cryted, origData)
+	return cryted
+}
+
+// PKCS7Padding 补码
+func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
+	padding := blocksize - len(ciphertext)%blocksize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
---
+++ redis/func.go
@@ -0,0 +1,43 @@
+package redis
+
+import (
+	redigo "github.com/gomodule/redigo/redis"
+)
+
+// Set 设置值
+func Set(key string, content interface{}, ex ...int64) (string, error) {
+	c := pool.Get()
+	defer c.Close()
+	if len(ex) == 0 || ex[0] == 0 {
+		return redigo.String(c.Do("SET", key, content))
+	}
+	return redigo.String(c.Do("SET", key, content, "EX", ex[0]))
+}
+
+// Expire 设置过期时间
+func Expire(key string, ex int64) (int64, error) {
+	c := pool.Get()
+	defer c.Close()
+	return redigo.Int64(c.Do("EXPIRE", key, ex))
+}
+
+// Get 获取值
+func Get(key string) ([]byte, error) {
+	c := pool.Get()
+	defer c.Close()
+	return redigo.Bytes(c.Do("GET", key))
+}
+
+// Exists Key存在
+func Exists(key string) (bool, error) {
+	c := pool.Get()
+	defer c.Close()
+	return redigo.Bool(c.Do("EXISTS", key))
+}
+
+// Delete Key删除
+func Delete(key string) (int64, error) {
+	c := pool.Get()
+	defer c.Close()
+	return redigo.Int64(c.Do("DEL", key))
+}
