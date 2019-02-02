0134366566fb8da11e681ae407e3e099d60e1ac3
BaiYang <xliu779@foxmail.com>
2019-02-02 17:52:16 +0800

    增加bcrypt加密验证方法

---
+++ encry/md5.go
@@ -0,0 +1,13 @@
+package encry
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// MD5 MD5
+func MD5(text string) string {
+	h := md5.New()
+	h.Write([]byte(text))
+	return hex.EncodeToString(h.Sum(nil))
+}
---
+++ encry/password.go
@@ -0,0 +1,21 @@
+package encry
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword 计算密码hash值
+func HashPassword(pwd string) (string, error) {
+	pwdBytes := []byte(pwd)
+	hashPwd, err := bcrypt.GenerateFromPassword(pwdBytes, bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashPwd), nil
+}
+
+// VerifyPassword 验证密码
+func VerifyPassword(pwd string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pwd))
+	return err == nil
+}
