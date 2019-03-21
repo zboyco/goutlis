ba7adbe1ca5660f2e8fdabca4cddfa762f507ce7
BaiYang <xliu779@foxmail.com>
2019-03-21 14:15:27 +0800

    增加单向链表实现

---
+++ list/singlyLinkedList.go
@@ -0,0 +1,120 @@
+package list
+
+import (
+	"errors"
+	"sync"
+)
+
+type singlyNode struct {
+	value interface{}
+	next  *singlyNode
+}
+
+// SinglyList 单向链表
+type SinglyList struct {
+	lenght int
+	front  *singlyNode
+	rear   *singlyNode
+	sync.RWMutex
+}
+
+// Lenght 当前长度
+func (list *SinglyList) Lenght() int {
+	return list.lenght
+}
+
+// Append 追加数据
+func (list *SinglyList) Append(item interface{}) error {
+	if item == nil {
+		return errors.New("数据不可为空")
+	}
+	list.Lock()
+	defer list.Unlock()
+	newNode := &singlyNode{value: item}
+
+	if list.front == nil {
+		list.front = newNode
+	}
+	if list.rear != nil {
+		list.rear.next = newNode
+	}
+	list.rear = newNode
+	list.lenght++
+	return nil
+}
+
+// Get 查找元素
+func (list *SinglyList) Get(index int) (item interface{}, err error) {
+	list.RLock()
+	defer list.RUnlock()
+	if index < 0 || list.lenght <= index {
+		err = errors.New("索引超出界限")
+		return
+	}
+	item = list.findNode(index).value
+	return
+}
+
+// InsertAt 插入数据
+func (list *SinglyList) InsertAt(index int, item interface{}) error {
+	list.Lock()
+	defer list.Unlock()
+	if list.lenght == 0 {
+		return errors.New("空链表无法插入")
+	}
+	if index < 0 || list.lenght <= index {
+		return errors.New("索引超出界限")
+	}
+	newNode := &singlyNode{value: item}
+	if index == 0 {
+		newNode.next = list.front
+		list.front = newNode
+	} else {
+		preNode := list.findNode(index - 1)
+		newNode.next = preNode.next
+		preNode.next = newNode
+	}
+	list.lenght++
+	return nil
+}
+
+// Remove 移除元素
+func (list *SinglyList) Remove(index int) error {
+	list.Lock()
+	defer list.Unlock()
+	if index < 0 || list.lenght <= index {
+		return errors.New("索引超出界限")
+	}
+	if index == 0 {
+		list.front = list.front.next
+		if list.lenght == 1 {
+			list.rear = nil
+		}
+	} else {
+		preNode := list.findNode(index - 1)
+		preNode.next = preNode.next.next
+		if index == (list.lenght - 1) {
+			list.rear = preNode
+		}
+	}
+	list.lenght--
+	return nil
+}
+
+// Clear 清空链表
+func (list *SinglyList) Clear() {
+	list.Lock()
+	defer list.Unlock()
+	list.front = nil
+	list.rear = nil
+	list.lenght = 0
+	return
+}
+
+func (list *SinglyList) findNode(index int) (n *singlyNode) {
+	n = list.front
+	for i := 0; i < index; i++ {
+		n = n.next
+	}
+	return
+}
---
+++ list/singlyLinkedList_test.go
@@ -0,0 +1,74 @@
+package list_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/zboyco/goutlis/list"
+)
+
+var l *list.SinglyList
+
+func init() {
+	l = &list.SinglyList{}
+}
+
+func TestAppend(t *testing.T) {
+	t.Log("测试追加")
+	for i := 0; i < 2; i++ {
+		err := l.Append("test text" + strconv.Itoa(i))
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestLenght(t *testing.T) {
+	t.Log("测试长度获取")
+	lenght := l.Lenght()
+	if lenght != 2 {
+		t.Error("长度不正确")
+	}
+}
+
+func TestInsertAt(t *testing.T) {
+	t.Log("测试插入数据")
+	err := l.InsertAt(1, "insert text3")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGet(t *testing.T) {
+	t.Log("测试获取元素")
+	item, err := l.Get(1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(item.(string))
+}
+
+func TestRemove(t *testing.T) {
+	t.Log("测试删除")
+	err := l.Remove(1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	item, err := l.Get(1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(item.(string))
+}
+
+func TestClear(t *testing.T) {
+	t.Log("测试清空")
+	l.Clear()
+	if l.Lenght() > 0 {
+		t.Error("清空失败")
+		return
+	}
+}
