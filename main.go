e5b60c962d9f642d664c3524d42df40df53f0b5f
BaiYang <xliu779@foxmail.com>
2019-04-04 18:40:31 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "github.com/zboyco/goutlis/encry"
+	_ "github.com/zboyco/goutlis/list"
+	_ "github.com/zboyco/goutlis/queue"
+	_ "github.com/zboyco/goutlis/redis"
+)
+
+func main() {
+
+}
